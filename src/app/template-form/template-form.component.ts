258380361fafa3919285256619f8499bddf7050e
juiceghost <jg@renegade.se>
2021-03-09 11:52:59 +0100

    Tek Tutorial: Template-driven forms done

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,25 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
   styleUrls: ['./template-form.component.css']
 })
+
+
+
 export class TemplateFormComponent implements OnInit {
   constructor() { }
-  title = 'Template driven forms';
+  @ViewChild('contactForm') contactForm: NgForm;
 
+  title = 'Template driven forms';
+  contact: contact;
   countryList:country[] = [
     new country('1', 'Norway'),
     new country('2', 'Denmark'),
     new country('3', 'Sweden')
   ];
 
-  onSubmit(contactForm) {
-    console.log(contactForm.value);
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+
+  changeAddress() {
+    let obj = {
+      city: "Bangalore",
+      street: "Brigade Road",
+      pincode: "600100"
+    };
+    let address = this.contactForm.controls["address"] as FormGroup
+    address.patchValue(obj);
+ 
+  }
+ 
+  changeCountry() {
+    this.contactForm.controls["country"].setValue("3");
+  }
+  reset() {
+    this.contactForm.resetForm();
+  }
+  resetForm() {
+    this.contactForm.resetForm();
+  }
+  patchValue() {
+    let obj = {
+      firstname: "Dr. No",
+      lastname: "Fransson",
+      email: "rahul@gmail.com",
+    };
+ 
+    this.contactForm.control.patchValue(obj);
+ 
   }
   
+  setDefaults() {
+    this.contact = {
+      firstname: "Dr. Krille",
+      lastname: "Tendulkar",
+      email: "sachin@gmail.com",
+      gender: "male",
+      isMarried: true,
+      country: "2",
+      address: { city: "Mumbai", street: "Perry Cross Rd", pincode: "400050" }
+    };
+  }
+
   ngOnInit(): void {
+    this.contact = {
+      firstname: "Dr. Krille",
+      lastname: "Tendulkar",
+      email: "sachin@gmail.com",
+      gender: "male",
+      isMarried: true,
+      country: "2",
+      address: { city: "Mumbai", street: "Perry Cross Rd", pincode: "400050" }
+    };
+    setTimeout(() => { 
+      this.contactForm.setValue(this.contact);
+    });
   }
 
 }
@@ -32,4 +93,18 @@ export class country {
     this.name=name;
   }
 }
+export class contact {
+  firstname:string;
+  lastname:string;
+  email:string;
+  gender:string;
+  isMarried:boolean;
+  country:string;
+  address: {
+    city:string;
+    street:string;
+    pincode:string;
+  }
+} 
+ 
  
